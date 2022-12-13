9f98cafb57dd4a88189e9cc6a997c87e998ffd39
Thanhdev394 <thanhdeveloper394@gmail.com>
2022-12-13 10:53:31 +0700

    patch

---
+++ bai1de3_patch/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+typedef long long ll;
+ll n,m;
+signed main()
+{
+    freopen("bai1.inp","r",stdin);
+    freopen("bai1.out","w",stdout);
+    cin >> n >> m;
+    cout << (n+1)*m + (m+1)*n;
+    return 0;
+}
