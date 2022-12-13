149f10e3a4f27c59feafa08e162a3fc1e4ee72ff
Thanhdev394 <thanhdeveloper394@gmail.com>
2022-12-13 10:53:07 +0700

    dapan

---
+++ bai1de3/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <math.h>
+#include <iostream>
+using namespace std;
+int n, m;
+int main()
+{
+    freopen("HANGRAO.inp", "r", stdin);
+    freopen("HANGRAO.out", "w", stdout);
+    cin >> m >> n;
+    cout << m * 2 * m + n * n;
+    return 0;
+}
